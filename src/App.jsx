ebde803e81b3adcd942689a8abe734a5c1f61b29
Zak Ward <zakward85@gmail.com>
2024-07-05 23:06:06 -0500

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,28 @@ import { useState } from 'react'
 
 
 function App() {
-//JS
+  //JS
+  //Challenge:
+  //Lets add to our JSX. 
+  //Create an ordered list under the <h1>
+  //Inside the ordered list insert two <li>'s
+  // each li should contain reasons you want to learn React.
 
-//Challenge:
-//Lets add to our JSX an <ol> 
-//with a few nested <li>'s of why you are excited to learn React
+  const username = "John Doe"
 
-const username = "John Doe"
-
-const userStyles = {
-  backgroundColor: "blue",
-  color: "white"
-}
+  const userStyles = {
+    backgroundColor: "blue",
+    color: "white"
+  }
   return (
-//JSX
-<>
-  <h1 style = {userStyles}>Welcome, {username}</h1>
-  <ol>
-    <li>Its a popular library, so I'll be in the know</li>
-    <li>I'm more likely to get a job if I know React</li>
-  </ol>
-</>
+    //JSX
+    <>
+      <h1 style={userStyles}>Welcome, {username}</h1>
+      <ol>
+        <li>It's a popular library, so I'll be in the know</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </>
   )
 }
 
